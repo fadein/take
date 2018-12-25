9a108c78e03c5ef32932e2d954ae41917e325573
Erik on Voyager2 <ewfalor@gmail.com>
2018-12-24 22:48:36 -0700

    avoid inf. loop with 'take 6 seconds' or 'take 7 seconds to'

--- take.scm
+++ take.scm
@@ -1,10 +1,12 @@
 #!/usr/bin/csi -s
 
-(import srfi-1
+(import ansi-escape-sequences
+        srfi-1
         srfi-13
         srfi-14
         (chicken string)
         (chicken file)
+        (chicken format)
         (chicken io)
         (chicken random)
         (chicken pathname)
@@ -25,13 +27,14 @@
 
 
 
-; take 5 minutes to ... then take 30 seconds to ...
+; parse command-line arguments given in the form of "take 5 minutes to ... then take 30 seconds to ..."
 ;
 ; '(((time 300) (to ...))
 ;   ((time 30) (to ...)))
-
+; TODO - these functions need better names
 (define (state0 args)
-  ;; look for a number, skip over words like "then" "take"
+  (print "state0:" args)   ; DELETE ME
+  ;; look for a number followed by a time quantifier, skip over (filler) words like "then", "take"
   (if (null? args)
       '()
       (let* ((this-arg (car args))
@@ -49,40 +52,48 @@
                               ((day    days)   86400)
                               (else                1))))
                         ((action consumed) (state1 (cddr args))))
+
+             (printf "from state1 I got (~a ~a)~n" action consumed)  ; DELETE ME
+
              (cons `((time ,time-spec) (to ,@action))
-                   (state0 (drop args consumed)))))
+                   (state0 (drop (cddr args) consumed)))))
 
           (else
+            (print "state0: else")  ; DELETE ME
             (state0 (cdr args)))))))
 
+
 (define (state1 args)
+  (define (state1-helper args consumed)
+    (print "  state1:" args)   ; DELETE ME
 
-  ; remove an initial "to"
-  (if (string-ci= (car args) "to")
-      (state1 (cdr args))
+    ; remove an initial "to"
+    (if (and (not (null? args)) (string-ci= (car args) "to"))
+        (state1-helper (cdr args) (add1 consumed))
 
-      (let loop ((args args) (action '()) (consumed 0))
-        (if (null? args)
-            (values (reverse action) consumed)
+        (let loop ((args args) (action '()) (consumed consumed))
+          (if (null? args)
+              (values (reverse action) consumed)
 
-            (let* ((this-arg (car args))
-                   (this-num (string->number this-arg))
-                   (this-sym (string->symbol this-arg)))
+              (let* ((this-arg (car args))
+                     (this-num (string->number this-arg))
+                     (this-sym (string->symbol this-arg)))
 
-              (cond
-                ;; if I see a number, transition back to state0 without consuming it
-                ((number? this-num)
-                 (values (reverse action) consumed))
+                (cond
+                  ;; if I see a number, transition back to state0 without consuming it
+                  ((number? this-num)
+                   (values (reverse action) consumed))
 
-                ;; skip over filler words like "take", "then", "to"
-                ((or (string-ci= this-arg "take")
-                     (string-ci= this-arg "then"))
-                 (loop (cdr args) action (add1 consumed)))
+                  ;; skip over filler words like "take", "then", "to"
+                  ((or (string-ci= this-arg "take")
+                       (string-ci= this-arg "then"))
+                   (loop (cdr args) action (add1 consumed)))
 
-                ;; else, accumulate into a list
-                (else
-                  (loop (cdr args) (cons this-arg action) (add1 consumed)))))))))
+                  ;; else, accumulate into a list
+                  (else
+                    (loop (cdr args) (cons this-arg action) (add1 consumed)))))))))
 
+  (state1-helper args 0))
 
 
 
