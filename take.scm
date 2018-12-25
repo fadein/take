53dfca36c0f03402329e47cd4114e7ae9485b84e
Erik on Voyager2 <ewfalor@gmail.com>
2018-12-24 22:27:40 -0700

    parses arguments almost to my liking; just too aggressive w/rt 'to'

--- take.scm
+++ take.scm
@@ -40,24 +40,46 @@
           ;; keep numeric quantities, look for a time specifier following the
           ;; qunantity and convert into seconds
           ((and (number? this-num) (>= this-num 0) (not (null? (cdr args))))
-           (cons (list 'time (* this-num
-                                (case (string->symbol (cadr args))
-                                  ((minute minutes)   60)
-                                  ((second seconds)    1)
-                                  ((hour   hours)   3600)
-                                  ((day    days)   86400)
-                                  (else                1)))
-                       (list 'to (state1 (cdr args))))))
+           (let-values (((time-spec)
+                         (* this-num
+                                        (case (string->symbol (cadr args))
+                                          ((minute minutes)   60)
+                                          ((second seconds)    1)
+                                          ((hour   hours)   3600)
+                                          ((day    days)   86400)
+                                          (else                1))))
+                        ((action consumed) (state1 (cddr args))))
+             (cons `((time ,time-spec) (to ,@action))
+                   (state0 (drop args consumed)))))
 
           (else
             (state0 (cdr args)))))))
 
 (define (state1 args)
-  (if (null? args)
-      '()
-      #f))
+      (let loop ((args args) (action '()) (consumed 0))
+        (if (null? args)
+            (values (reverse action) consumed)
 
-      
+            (let* ((this-arg (car args))
+                   (this-num (string->number this-arg))
+                   (this-sym (string->symbol this-arg)))
+
+              (cond
+                ;; if I see a number, transition back to state0 without consuming it
+                ((number? this-num)
+                 (values (reverse action) consumed))
+
+                ;; skip over filler words like "take", "then", "to"
+                ((or (string-ci= this-arg "take")
+                     (string-ci= this-arg "then")
+                     (string-ci= this-arg "to"))
+                 (loop (cdr args) action (add1 consumed)))
+
+                ;; else, accumulate into a list
+                (else
+                  (loop (cdr args) (cons this-arg action) (add1 consumed))))))))
+
+
 
 
 (parse-command-line (command-line-arguments))
